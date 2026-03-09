b0ae557c9420e0320d1c8fc3181e98a512076b55
Manfred Gawlas <manfredgawlas12@gmail.com>
2026-03-09 14:55:58 +0100

    Added insertion sort

--- examples/cpp_sort_tests/main.cpp
+++ examples/cpp_sort_tests/main.cpp
@@ -1,12 +1,42 @@
 #include <cclib_cpp/cclib_cpp.hpp>
 
 #include <vector>
+#include <iostream>
 
 template <typename T>
 void insertion_sort(std::vector<T>& data) {
+    size_t n = data.size();
+
+    for (int i = 1; i < n; i++) {
+        T key = data[i];
+
+        // Here size_t fucks up the program since we have j = -1 at some point after while.
+        int j = i-1;
+
+        while(j >= 0 && data[j] < key) {
+            // std::cout << "deb: " << j << " " << data[j] << " " << key << std::endl;
+            data[j+1] = data[j];
+
+            j--;
+        }
+        data[j+1] = key;
+    }
+
 }
 
 int main() {
     cclib::cpp::hello();
+
+     std::vector<int> test1 = {4, 5, 3, 2, 1};
+    //std::vector<int> test1 = {1, 2, 3,5, 4};
+
+    for (const auto& i : test1) std::cout << i << " ";
+    std::cout << std::endl;
+
+    insertion_sort(test1);
+
+    for (const auto& i : test1) std::cout << i << " ";
+    std::cout << std::endl;
+
 
 }
